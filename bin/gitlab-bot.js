ccc57cbc51f414253dcc22e62a93b6f2d9a545f0
0zean <0zean@live.com>
2017-11-28 17:06:26 +0100

    added gitignore, added webhook integration

--- bin/gitlab-bot.js
+++ bin/gitlab-bot.js
@@ -4,6 +4,8 @@ const program = require('commander');
 
 program
     .option('-p, --port <n>', 'Port to start the server on', process.env.PORT || 3000)
+    .option('-w, --webhook-path <path>', 'URL path which receives webhooks. Ex: /webhook', process.env.WEBHOOK_PATH)
+    .option('-s, --secret <secret>', 'Webhook secret of the GitLab App', process.env.WEBHOOK_SECRET)
     .parse(process.argv);
 
 const createBot = require('../');
--- server.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-
-server = http.createServer((req, res) => {
-    res.end('Hello World');
-});
-
-server.listen(8080);
-
